c3000ee6c2fccc5912be1a76762f9e87cdd52025
JugaadChhabra <jugaad.chhabra@gmail.com>
2025-09-20 23:55:56 +0530

    made minor changes

---
+++ src/components/forms/index.ts
@@ -0,0 +1,11 @@
+// Form components exports
+// Reusable form components
+
+// Example exports (to be added):
+// export { Input } from './Input';
+// export { Button } from './Button';
+// export { Select } from './Select';
+// export { Toggle } from './Toggle';
+// export { FormField } from './FormField';
+
+export {};
---
+++ src/components/layout/index.ts
@@ -0,0 +1,10 @@
+// Layout components exports
+// For navigation, headers, footers, etc.
+
+// Example exports (to be added):
+// export { Navbar } from './Navbar';
+// export { Footer } from './Footer';
+// export { Sidebar } from './Sidebar';
+// export { MainLayout } from './MainLayout';
+
+export {};
